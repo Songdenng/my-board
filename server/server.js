85c2aea900097a76c18b59b34c499d3d81bc30c8
hansong401@gmail.com <98142480+Songdenng@users.noreply.github.com>
2022-03-30 19:40:57 +0900

    서버&클라이언트 연동
    서버와 클라이언트 동시 실행 가능

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const PORT = process.eventNames.PORT || 3001;
-
-app.get('/', (req,res) => {
-    res.send(`Hello SongD`);
-});
+app.get('/api/host', (req, res) => {
+    res.send({ host : 'SongD' });
+})
 
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
 })
+
